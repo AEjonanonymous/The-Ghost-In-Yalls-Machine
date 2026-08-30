3d2390b678a7057085920488128c37c8750bfcb7
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-08-29 21:39:43 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,12 @@ open Lake DSL
 
 package «TheGhostInYallsMachine» where
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
 @[default_target]
-lean_lib «TheGhostInYallsMachine» where
+lean_lib «theorem_hessian_rank_deficiency» where
+  srcDir := "."
+
+lean_lib «OPN-Proof2» where
   srcDir := "."
